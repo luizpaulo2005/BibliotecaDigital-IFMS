45b84c51000ddad537df7e2d132cef9ec250cf3a
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2022-09-24 18:07:14 -0400

    Header Modificado/Data posta no padrão dia/mês/ano

--- pages/components/header/paginicial.js
+++ pages/components/header/paginicial.js
@@ -1,8 +1,8 @@
 export default function HDPagInicial(){
     return(
         <nav className="navbar navbar-expand-lg bg-success">
-        <div className="container-fluid">
-        <a className="navbar-brand" href="#">Biblioteca Digital de Pesquisas - IFMS</a>
+        <div className="container">
+        <a className="navbar-brand" href="#"><span className="titleheader">Biblioteca Digital de Pesquisas - IFMS</span></a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
         </button>
