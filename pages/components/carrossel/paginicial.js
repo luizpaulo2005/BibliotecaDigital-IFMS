6529e922a9a88266bc403b1676a820b1a367cfcf
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2022-10-12 16:00:41 -0300

    Carousel funcionando.

--- pages/components/carrossel/paginicial.js
+++ pages/components/carrossel/paginicial.js
@@ -1,36 +1,33 @@
 import Image from "next/image";
+import { format, parseISO } from "date-fns";
 
-export default function CRSPagInicial(){
+
+
+export default function CRSPagInicial({pesquisas}){
     return(
         <div id="carouselExampleCaptions" className="carousel slide carrossel border rounded" data-bs-ride="false">
+            
         <div className="carousel-inner">
-        <div className="carousel-item active" data-bs-interval="3000">
-        <div className="d-block w-100">
+        {pesquisas.map(({id, titulo, discenteId, docenteId, data_apresentacao})=> (
+        <div key={pesquisas} className="carousel-item active" data-bs-interval="3000">
+        <div className="d-flex w-100">
         <Image src="https://www.ifms.edu.br/imagens/icones/s/1/ifms" alt="Image" width={800} height={488}/>
         </div>
-        <div className="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+        <div className="carousel-caption d-none d-md-block ">
+       
+          <div key={id}>
+          <div>{titulo}</div>
+          <div>{discenteId}</div>
+          <div>{docenteId}</div>
+          <div>{format(parseISO(data_apresentacao), 'dd/MM/yyyy')}</div>
+          <div>
+          </div>
+          </div>
+        
         </div>
         </div>
-        <div className="carousel-item active" data-bs-interval="2000">
-        <div className="d-block w-100">
-        <Image src="https://www.ifms.edu.br/imagens/icones/s/1/ifms" alt="Image" width={800} height={488}/>
-        </div>
-        <div className="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-        </div>
-        </div>
-        <div className="carousel-item active" data-bs-interval="2000">
-        <div className="d-block w-100">
-        <Image src="https://www.ifms.edu.br/imagens/icones/s/1/ifms" alt="Image" width={800} height={488}/>
-        </div>
-        <div className="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-        </div>
-        </div>
+        ))}
+
         </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,21 @@ import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import CRSPagInicial from './components/carrossel/paginicial'
 import HDPagInicial from './components/header/paginicial'
+import axios from "axios";
 
-export default function Home() {
+
+export const getStaticProps = async () => {
+  const response = await axios.get('https://databasebibliotecadigital.undertak3r.repl.co/pesquisa')
+  const pesquisas = await response.data
+  return {
+    props: {
+      pesquisas
+    },
+    revalidate: 3600
+  }
+}
+
+export default function Home({pesquisas}) {
   return (
     <div className='container-fluid g-0'>
 
@@ -17,7 +30,7 @@ export default function Home() {
     <HDPagInicial/>
     </div>
     <div className={styles.main}>
-    <CRSPagInicial/>
+    <CRSPagInicial pesquisas={pesquisas}/>
     <nav className="navbar bg-light">
   <div className="container-fluid textcenter">
     <form className="d-flex" role="search">
