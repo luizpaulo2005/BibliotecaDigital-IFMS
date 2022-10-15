2ddd63f372b57f81885a9106675fabe095efcee4
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2022-10-15 15:29:28 -0300

    curso solo

---
+++ pages/posts/user/solo/curso/[id].js
@@ -0,0 +1,40 @@
+import axios from "axios"
+import Head from "next/head"
+import Link from "next/link"
+import HDPagInicial from "../../../../components/header/paginicial"
+
+export const getServerSideProps = async (context) => {
+    const id = context.query.id
+    const response = await axios.get(`https://databasebibliotecadigital.undertak3r.repl.co/curso/${id}`)
+    const attributes = await response.data
+    return {
+        props: {
+            attributes
+        }
+    }
+}   
+
+export default function SoloCurso({attributes}){
+    return(
+        <div className="container-fluid g-0">
+            <Head>
+                <title>{attributes.nome}</title>
+            </Head>
+            <HDPagInicial/>
+            <div className="container rounded p-3 mt-2">
+            <div className="card">
+                <div className="card-header">
+                Nome: {attributes.nome}
+                </div>
+                <div className="card-body">
+                    <p>Grade: {attributes.grade}</p>
+                    <p>Duração: {attributes.duracao}</p>
+                    <p>Campus: {attributes.campusId}</p>
+                    <Link href=""><a></a></Link>
+                </div>
+            </div>           
+            </div>
+
+        </div>
+    )
+}
