4e56d536fb034fb8043b11288af7c2fb9c98b112
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2022-10-18 15:52:26 -0300

    class name

--- pages/components/footer/footertable.js
+++ pages/components/footer/footertable.js
@@ -1,9 +1,9 @@
 export default function FooterTable(){
-    <footer class="bg-light text-center text-lg-start">
-  <div class="container p-4">
-    <div class="row">
-      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Footer text</h5>
+    <footer className="bg-light text-center text-lg-start">
+  <div className="container p-4">
+    <div className="row">
+      <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 className="text-uppercase">Footer text</h5>
 
         <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
@@ -11,8 +11,8 @@ export default function FooterTable(){
           aliquam voluptatem veniam, est atque cumque eum delectus sint!
         </p>
       </div>
-      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Footer text</h5>
+      <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 className="text-uppercase">Footer text</h5>
 
         <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
@@ -23,9 +23,9 @@ export default function FooterTable(){
     </div>
   </div>
 
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+  <div className="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
     © 2020 Copyright:
-    <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    <a className="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
   </div>
 </footer>
 }
