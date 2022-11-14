632d16246e9537bc4cdce549f0adc19664edea24
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2022-11-14 14:03:55 -0300

    Rotas(authcontext&authreducer)

--- pages/posts/admin/login/login.js
+++ pages/posts/admin/login/login.js
@@ -6,40 +6,8 @@ import { useRouter } from "next/router";
 import { initializeApp } from "firebase/app";
 import Head from "next/head";
 import HDPagInicial from "../../../../components/header/paginicial";
+import { AuthContext, AuthReducer } from "../../../../components/AuthContext&ReducerContext/AuthFunctions";
 
-
-export const AuthContextProvider = ({children}) => {
-  const [autenticacao, setAutenticacao] = useReducer(AuthReducer, UsuarioInicial)
-
-  return (
-      <AuthContext.Provider value={{usuario : autenticacao.usuario, setAutenticacao}}>
-          {children}
-      </AuthContext.Provider>
-  )
-}
-
-const UsuarioInicial = {
-  usuario : null
-}
-
-export const AuthContext = createContext(UsuarioInicial);
-
-export const AuthReducer = (autenticacao, tipo) => {
-  switch (tipo.type){
-      case "LOGIN": {
-          return  {
-              usuario:tipo.payload,
-          }
-      }
-      case "LOGOUT":{
-return {
-  usuario : null,
-}
-      }
-
-      default: return autenticacao;
-  }
-} 
 
 export default function Login() {
   const [error, setError] = useState(false);
