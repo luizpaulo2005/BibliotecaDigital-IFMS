40673ecbe8bc866df1fc743136d29f9fa908b0a5
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2022-12-14 17:23:07 -0300

    offline page

--- pages/_offline.js
+++ pages/_offline.js
@@ -4,7 +4,11 @@ const Offline = () => {
   return (
     <div className="container-fluid g-0">
         <HeaderUser/>
-        <div>Você está Offline</div>;
+        <div className="mt-2 d-flex justify-content-center">
+          <h1>
+          Você está Offline
+          </h1>
+          </div>;
     </div>
   )
 };
